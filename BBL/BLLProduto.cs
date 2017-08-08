1190a767b04e3f2b078dbcfbe2a502f8c7f9034c
brncolucci <brunocolucci03@hotmail.com>
2017-08-08 13:01:42 -0300

    AbrilBackend
    TesteAbril

---
+++ BBL/BLLProduto.cs
@@ -0,0 +1,138 @@
+﻿using DAL;
+using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BLLProduto
+    {
+        private DALConexao CONEXAO;
+
+        public BLLProduto(DALConexao con) 
+        {
+            this.CONEXAO = con;
+        }
+
+        public void Incluir(ModeloProduto p) 
+        {
+            if (p.DescricaoProduto.Trim().Length == 0)
+            {
+                throw new Exception("A descrição do Produto é obrigatoria.");
+            }
+
+            if (p.NomeProduto.Trim().Length == 0) 
+            {
+                throw new Exception("O nome do produto é obrigatório.");
+            }
+
+            if (p.ValorVenda <= 0) 
+            {
+                throw new Exception("O valor da venda deve ser maior que zero.");
+            }
+
+            if (p.Quantidade <= 0)
+            {
+                throw new Exception("A quantidade deve ser maior que zero.");
+            }
+
+            if (p.Quantidade > 1000) 
+            {
+                throw new Exception("Valor maximo da quantidade alcaçado para esta operação, o valor deve ser menor ou igual a mil.");
+            }
+
+            if (p.UnidadeMedida <= 0) 
+            {
+                throw new Exception("Unidade de medida deve ser informada.");
+            }
+
+            if (p.Categoria <= 0) 
+            {
+                throw new Exception("A Categoria deve ser informada.");
+            }
+
+            if (p.SubCategoria <= 0) 
+            {
+                throw new Exception("A SubCategoria deve ser informada.");
+            }
+
+            DALProduto da = new DALProduto(CONEXAO);
+            da.Incluir(p);
+        }
+
+        public void Alterar(ModeloProduto p) 
+        {
+            if (p.CodigoProduto <= 0) 
+            {
+                throw new Exception("Selecione um produto para alterar.");
+            }
+
+            if (p.DescricaoProduto.Trim().Length == 0)
+            {
+                throw new Exception("A descrição do Produto é obrigatoria.");
+            }
+
+            if (p.NomeProduto.Trim().Length == 0)
+            {
+                throw new Exception("O nome do produto é obrigatório.");
+            }
+
+            if (p.ValorVenda <= 0)
+            {
+                throw new Exception("O valor da venda deve ser maior que zero.");
+            }
+
+            if (p.Quantidade <= 0)
+            {
+                throw new Exception("A quantidade deve ser maior que zero.");
+            }
+
+            if (p.Quantidade > 1000)
+            {
+                throw new Exception("Valor maximo da quantidade alcaçado para esta operação, o valor deve ser menor ou igual a mil.");
+            }
+
+            if (p.UnidadeMedida <= 0)
+            {
+                throw new Exception("Unidade de medida deve ser informada.");
+            }
+
+            if (p.Categoria <= 0)
+            {
+                throw new Exception("A Categoria deve ser informada.");
+            }
+
+            if (p.SubCategoria <= 0)
+            {
+                throw new Exception("A SubCategoria deve ser informada.");
+            }
+
+            DALProduto da = new DALProduto(CONEXAO);
+            da.Alterar(p);
+        }
+
+        public void Excluir(int codigo) 
+        {
+            DALProduto da = new DALProduto(CONEXAO);
+            da.Excluir(codigo);
+        }
+
+        public DataTable Localizar(string s) 
+        {
+            DALProduto da = new DALProduto(CONEXAO);
+
+            return da.Localizar(s);
+        }
+
+        public ModeloProduto carregaProduto(int codigo) 
+        {
+            DALProduto da = new DALProduto(CONEXAO);
+
+            return da.carregaProduto(codigo);
+        }
+    }
+}
---
+++ BBL/BLLUnidadeMedida.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+using DAL;
+using System.Data;
+
+namespace BLL
+{
+    public class BLLUnidadeMedida
+    {
+        private DALConexao CONEXAO;
+
+        public BLLUnidadeMedida(DALConexao con) 
+        {
+            this.CONEXAO = con;
+        }
+
+        private bool UnidadeMedidaExiste(ModeloUnidadeMedida um) 
+        {
+            bool retorno;
+            DataTable tabela = new DataTable();
+            tabela = this.Localizar(um.UnidadeMedidaNome);
+
+            if (tabela.Rows.Count > 0)
+            {
+                if (tabela.Rows[0]["umed_nome"].ToString().ToUpper() == um.UnidadeMedidaNome.ToUpper())
+                    retorno = true;
+                else
+                    retorno = false;                
+            }
+            else
+                retorno = false;
+
+            return retorno;
+        }
+
+        public void Incluir(ModeloUnidadeMedida um)
+        {
+            if (um.UnidadeMedidaNome.Trim().Length == 0) //Verifica o nome.
+            {
+                throw new Exception("O nome da Unidade de Medida é obrigatorio.");
+            }
+
+            if (this.UnidadeMedidaExiste(um)) //Verifica se ja existe uma unidade de medida cadastrada.
+            {
+                throw new Exception("Unidade de medida ja cadastrada.");
+            }
+
+            DALUnidadeMedida da = new DALUnidadeMedida(CONEXAO);
+            da.Incluir(um);
+        }
+
+        public void Alterar(ModeloUnidadeMedida um)
+        {
+            if (um.UnidadeMedidaCodigo <= 0) //Selecionar uma unidade de medida para operação.
+            {
+                throw new Exception("Selecione uma Unidade de Medida.");
+            }
+
+            if (um.UnidadeMedidaNome.Trim().Length == 0) //Verifica o nome.
+            {
+                throw new Exception("O nome da Unidade de Medida é obrigatorio.");
+            }
+
+            if (this.UnidadeMedidaExiste(um)) //Verifica se ja existe uma unidade de medida cadastrada.
+            {
+                throw new Exception("Unidade de medida ja cadastrada.");
+            }
+
+            DALUnidadeMedida da = new DALUnidadeMedida(CONEXAO);
+            da.Alterar(um);
+        }
+
+        public void Excluir(int codigo)
+        {
+            DALUnidadeMedida da = new DALUnidadeMedida(CONEXAO);
+            da.Excluir(codigo);
+        }
+
+        public DataTable Localizar(String s)
+        {
+            DALUnidadeMedida da = new DALUnidadeMedida(CONEXAO);
+
+            return da.Localizar(s);
+        }
+
+        public ModeloUnidadeMedida carregaUnidadeMedida(int codigo)
+        {
+            DALUnidadeMedida da = new DALUnidadeMedida(CONEXAO);
+
+            return da.carregaUnidadeMedida(codigo);
+        }
+    }
+}
